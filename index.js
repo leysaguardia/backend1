0fe7d603e0abcfde735d696a884b590b561a2c9e
leyguardia <leyguardiaj@gmail.com>
2025-04-01 11:30:31 -0400

    agregue carpeta

---
+++ index.js
@@ -0,0 +1,29 @@
+// java permite la mutabilidad de las variables (perjudica a la larga en proyectos grandes)
+let nombre = "santiago"
+
+nombre = 12
+
+//console.log(nombre);
+
+//nombre= diego
+//console.log(nombre);
+
+
+//tipos de datos 
+
+//console.log(typeof nombre);
+
+//const obj = { clave: "valor"}
+ const obj = {nombre: "andres", apellidos: "sanchez"}
+//console.log(obj);
+//console.log(typeof obj);
+
+const array = [1, 2, 3, 4, 5, "hola", {nombre:"andres", apellidos:"sanchez"}, true]
+console.log(array);
+console.log(typeof array);
+  //No es mutable y genera error 
+const nombre_02 = "pablo";
+nombre_02 = "juan";    // las constantes una vez asigandas con un valor definido, no se pueden modificar
+
+
+
